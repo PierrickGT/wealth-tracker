2c4d0164d5b246d5b86fa0c9c129d31de45f0bde
Pierrick TURELIER <turelier.pierrick@gmail.com>
2018-09-13 13:49:03 +0200

    Add unit tests to navigation

--- src/components/menu/index.test.jsx
+++ src/components/menu/index.test.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import createRouterContext from 'react-router-test-context'
 import { shallow } from 'enzyme';
 
 import HeaderMenu from ".";
 
-describe('my test', () => {
-  it('renders', () => {
-    const context = createRouterContext()
-    const wrapper = shallow(<HeaderMenu />, { context })
-    const { mode } = wrapper.props();
+describe('<Menu />', () => {
+    HeaderMenu.contextTypes = {
+        router: PropTypes.shape({})
+    }
 
-    expect(mode).toBe('horizontal')
-  })
+    it('should display home tab by default', () => {
+        const context = createRouterContext()
+        const wrapper = shallow(<HeaderMenu />, { context })
+        const { current } = wrapper.state();
+
+        expect(current).toBe('home');
+    })
+
+    it('should display transactions tab', () => {
+        const context = createRouterContext()
+        const spy = jest.fn();
+        const wrapper = shallow(<HeaderMenu onClick={spy} />, { context });
+
+        expect(wrapper.state('current')).toBe('home');
+        wrapper.simulate('click', { key: 'transactions' })
+        expect(wrapper.state('current')).toBe('transactions');
+    })
 })
