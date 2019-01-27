81af19e204a6a2ba3f48ffccfafc62c8874b5fc6
Pierrick TURELIER <turelier.pierrick@gmail.com>
2019-01-27 13:27:52 +0100

    Setup codecov to upload report

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.test.{js,jsx}'],
+    coverageDirectory: './coverage/',
     setupTestFrameworkScriptFile: './setupJest.js',
     transform: {
         '^.+\\.(js|jsx)$': 'babel-jest'
     },
+    transformIgnorePatterns: [
+      '/node_modules/(?!antd)'
+    ],
     verbose: true
 };
